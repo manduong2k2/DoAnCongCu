4d5866412cb8c97a1c6c486d6879e2aed974d4b5
july2nd <manduong2k2@gmail.com>
2024-04-18 22:54:54 +0700

    init

---
+++ E-tech/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\brandController;
+use App\Http\Controllers\cartController;
+use App\Http\Controllers\categoryController;
+use App\Http\Controllers\orderController;
+use App\Http\Controllers\productController;
+use App\Http\Controllers\userController;
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\Authorize;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+
